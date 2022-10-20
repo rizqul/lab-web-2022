96c3800c95ff46cd958a4643563459aca6237b9d
sofyanox12 <gaero38@gmail.com>
2022-10-21 02:13:23 +0800

    tambah `animateCard()` dan `chat()`

--- H071211045/Tugas V/script.js
+++ H071211045/Tugas V/script.js
@@ -17,20 +17,21 @@ backCard.src = "assets/back_red.png";
 var pot = document.getElementById("total-pot").innerHTML;
 var player = document.getElementById("money").innerHTML;
 
-var mode = ["Poor", "Classic", "Professional", "Mafia", "Bilionaire"];
+var mode = ["Poor", "Classic", "Professional", "Mafia", "Billionaire"];
 
-var hiddenCard;
+var hiddenCard, deal;
 var haveAceP1, haveAceP2;
 var hit, deck, money;
 var startingMoney = 5000;
 
 function prepareGame() { // Mengatur game
     document.getElementById("bet").value = "";
+    document.getElementById("game-result").innerHTML = "";
     document.getElementById("player-deck").innerHTML = "";
     document.getElementById("dealer-deck").innerHTML = "";
     deck = [];
     dealer = player = bet = 0;
-    haveAceP1 = haveAceP2 = false;
+    haveAceP1 = haveAceP2 = deal = false;
     hit = true;
 }
 
@@ -77,13 +78,14 @@ makeDeck = () => { // Mengatur Kartu
         player += getValue(card);
     }
 
+
 }
 
 takeCard = () => { // Mengambil kartu tambahan
     hit = (player > 21 && hit == true) ? false : true;
     if (!hit) return;
     let card = deck.pop();
-    // let card = "AK";
+    // let card = "AK"; // -- debug ace
     addCard(card, "player-deck");
     player += getValue(card);
     document.getElementById("player-sum").innerHTML = player;
@@ -95,11 +97,12 @@ switchWindow = (origin, target) => { // Mengganti tampilan
     document.getElementsByClassName(target)[0].style = "display: block; z-inde:=50;";
 }
 
-addCard = (card, targetParent) => { // Menambahkan kartu ke dalam deck 
+addCard = (card, targetParent) => { // Menambahkan kartu ke dalam deck  
     let card_image = document.createElement("img");
     card_image.className = "cards drawn";
     card_image.src = "assets/cards/" + card + ".png";
     document.getElementById(targetParent).append(card_image);
+    return animateCard(targetParent);
 }
 
 getValue = (card) => { // Menghitung nilai kartu
@@ -125,6 +128,9 @@ updateStatus = () => { // Update status
     document.getElementById("total-pot").innerHTML = pot;
     document.getElementById("money").innerHTML = money;
 
+    document.getElementsByClassName("exit")[0].disabled = false;
+    document.getElementById("stay-button").disabled = false;
+    document.getElementById("take-button").disabled = false;
     if (bet > 0) bet = "";
 }
 
@@ -145,36 +151,49 @@ checkAce = (playerScore, targetParent, haveAce) => { // Mengecek apakah ada kart
 stand = () => { // Menyimpan nilai kartu (Seperti Submit)
     updateStatus();
     let belowLimit = (player <= 21 && dealer <= 21);
-    let botAI = Math.floor(Math.random() * 16) + 4;
+    let botAI = Math.floor(Math.random() * 16) + 3;
+
     hit = (hit == true) ? false : true;
 
     document.getElementById("dealer-deck").removeChild(backCard);
     addCard(hiddenCard, "dealer-deck");
 
-    while (dealer < botAI) { // 15-20
+    while (dealer < botAI) {
         let card = deck.pop();
         dealer += getValue(card);
         addCard(card, "dealer-deck");
     }
 
-    if ((belowLimit && (player > dealer)) || (dealer > 21 && player <= 21)) {
+    // Tombol di nonaktifkan sementara animasi berjalan
+    document.getElementById("stay-button").disabled = true;
+    document.getElementById("take-button").disabled = true;
+    document.getElementsByClassName("exit")[0].disabled = true;
+
+    let playerCount = document.getElementById("player-deck").children.length;
+    let dealerCount = document.getElementById("dealer-deck").children.length;
+
+    if ((belowLimit && ((player > dealer) || (playerCount >= 5))) || (dealer > 21 && player <= 21)) {
         document.getElementById("game-result").innerHTML = "You Win!";
         money += pot;
 
-    } else if ((belowLimit && (dealer > player)) || (player > 21 && dealer <= 21)) {
+    } else if ((belowLimit && ((dealer > player) || (dealerCount >= 5))) || (player > 21 && dealer <= 21)) {
         document.getElementById("game-result").innerHTML = "You Lose!";
-    
-    } else if ((player == 21)) {
+
+    } else if ((player == 21 && dealer != 21)) {
         document.getElementById("game-result").innerHTML = "Blackjack!";
         money += blackjack;
-    
+
     } else {
         money += Math.round(pot / 2);
         document.getElementById("game-result").innerHTML = "Draw!";
     }
 
-    updateStatus();
-    popUp("game-over", 1);
+    chat();
+    setTimeout(() => {
+        updateStatus();
+        popUp("game-over", 1);
+    }, 3000);
+
 }
 
 popUp = (window, index) => {
@@ -187,11 +206,13 @@ popUp = (window, index) => {
     }
 }
 
-button = {
+button = { // Kumpulan Tombol
     playAgain: $("#play-again").click(function () {
+        document.getElementById("game-result").innerHTML = "";
         popUp("game-over", 0);
         askBet();
         updateStatus();
+        chat();
     }),
 
     exit: $(".exit").click(function () {
@@ -202,6 +223,7 @@ button = {
         prepareGame();
         makeDeck();
         updateStatus();
+        chat();
     }),
 
     submit: $("#submit").click(function () {
@@ -216,9 +238,8 @@ button = {
             pot += bet;
             money -= bet;
             pot += (Math.floor(Math.random() < 0.5) && bet > 500 ? (bet + 250) : (bet)); // Uang dari dealer
-            
+            chat();
             updateStatus();
-            
             popUp("betting", 0);
 
         } else {
@@ -230,11 +251,11 @@ button = {
         money = startingMoney;
         updateStatus();
         popUp("reset-money", 0);
+        chat();
         askBet();
     }),
 
     gotoOptions: $("#goto-options").click(function () {
-        // document.getElementById("options").style = "background-color: rgb(18, 18, 18);";
         switchWindow("menu", "preferences");
     }),
 
@@ -247,9 +268,8 @@ button = {
 
     switchMode: $("#switch-mode").click(function () {
         let x = mode.indexOf(document.getElementById("money-mode").innerHTML) + 1;
-        if(x > mode.length - 1) x = 0;
+        if (x > mode.length - 1) x = 0;
         document.getElementById("money-mode").innerHTML = mode[x];
-        // Use case
         switch (x) {
             case 0:
                 startingMoney = 100;
@@ -270,8 +290,67 @@ button = {
         money = startingMoney;
     }),
 
-    goBack : $("#go-back").click(function () {
+    goBack: $("#go-back").click(function () {
         switchWindow("preferences", "menu");
     })
 }
+
+/*
+* Fitur Opsional
+*/
+
+chat = () => { // Chat bot dari dealer
+    let chat = document.getElementById("dealer-chat");
+    let condition = document.getElementById("game-result").innerHTML;
+
+    win = ["Damn you boy! I got " + dealer, "Just a card of " + dealer + ". You lucky bastard.",
+        "Spend that money on next round.", "I thought mine was bigger."];
+    lose = ["No luck for you today. This " + dealer + " is big.", "Poor man will always get poor.",
+    "You're so unlucky. Mine is only " + dealer];
+    draw = ["Draw? Really?", "Only on this time boy.", "LMAO, Draw.", "Why do this outcome is possible?"];
+    random = ["Are you sure, mortal?", "Gambit is not good for health", "You better luck this time."];
+
+    if (condition == "You Win!" || condition == "Blackjack!") {
+        chat.innerHTML = win[Math.floor(Math.random() * win.length)];
+    } else if (condition == "You Lose!") {
+        chat.innerHTML = lose[Math.floor(Math.random() * lose.length)];
+    } else if (condition == "Draw!") {
+        chat.innerHTML = draw[Math.floor(Math.random() * draw.length)];
+    } else {
+        chat.innerHTML = random[Math.floor(Math.random() * random.length)];
+    }
+}
+
+animateCard = async (deckName) => { // Animasi kartu dibagi
+    deckName = "." + deckName;
+
+    // Mengambil posisi x dari firstPlace
+    let xFirst = $("#back-card").offset().left;
+    let yFirst = $("#back-card").offset().top;
+
+    // Mengambil posisi x dari kartu terbaru
+    let xCard = $(deckName + " .drawn:last-child").offset().left;
+    let yCard = $(deckName + " .drawn:last-child").offset().top;
+
+    // Menyesuaikan posisi kartu terbaru dengan posisi firstPlace
+    $(deckName + " .drawn:last-child").offset({
+        top: yFirst,
+        left: xFirst
+    });
+
+    // Animasi dari firstPlace ke kartu terbaru
+    $(deckName + " .drawn:last-child").animate({
+        top: yCard - (yFirst - 30),
+        left: xCard - (xFirst - 30)
+    }, 600, function () {
+        // Penyetelan ke posisi awal agar kartu menyesuai lagi
+        $(deckName + " .drawn").css({
+            position: "relative",
+            top: 0,
+            left: 0
+        });
+        deal = false;
+    });
+}
+
 
