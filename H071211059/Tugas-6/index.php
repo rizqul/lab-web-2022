21396bd4c2599920b2a1d61994c9d1f7e1639a7e
erwinarif31 <muherwinarif31@gmail.com>
2022-11-06 17:25:45 +0800

    add `Add Product` modal using bootstrap

--- H071211059/Tugas-6/index.php
+++ H071211059/Tugas-6/index.php
@@ -11,7 +11,45 @@
 
 <body>
 
-
+    <div class="modal fade" id="productAddModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form id="saveStudent">
+                    <div class="modal-body">
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="title" name="title" placeholder="text">
+                            <label for="title">Title</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="price" name="price" placeholder="text">
+                            <label for="price">Price</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="qty" name="qty" placeholder="text">
+                            <label for="qty">Quantity</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="series" name="series" placeholder="text">
+                            <label for="series">Series</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="img-source" name="img-source"
+                                placeholder="text">
+                            <label for="img-source">Image Source</label>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save Product</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 
     <div class="container mt-4">
@@ -20,9 +58,8 @@
                 <div class="card">
                     <div class="card-header">
                         <h4>XY Store Products
-
                             <button type="button" class="btn btn-primary float-end" data-bs-toggle="modal"
-                                data-bs-target="#studentAddModal">
+                                data-bs-target="#productAddModal">
                                 Add Products
                             </button>
                         </h4>
