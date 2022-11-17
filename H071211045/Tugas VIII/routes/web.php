4c19b75e15c6aa600527115ff771a41b46d7d72f
sofyanox12 <gaero38@gmail.com>
2022-11-18 01:04:24 +0800

    installed bootstrap

--- H071211045/Tugas VIII/routes/web.php
+++ H071211045/Tugas VIII/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// Route::view('/about', 'about');
+// Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
