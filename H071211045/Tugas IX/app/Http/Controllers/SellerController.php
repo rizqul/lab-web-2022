d4a7acf3a1232031206d7b288491dd7bfd766e68
sofyanox12 <gaero38@gmail.com>
2022-11-29 10:59:28 +0800

    Menambah fungsi pada `Seller` dan `SellerPermissions` Controller

--- H071211045/Tugas IX/app/Http/Controllers/SellerController.php
+++ H071211045/Tugas IX/app/Http/Controllers/SellerController.php
@@ -2,9 +2,104 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sellers;
 use Illuminate\Http\Request;
+use \Carbon\Carbon as Date;
+use Illuminate\Support\Facades\DB;
 
 class SellerController extends Controller
 {
-    //
+    public function index() {
+        $sellers = Sellers::latest()->paginate(5);
+        return view('index');
+    }
+
+    public function storeSellerEloquent(Request $request) { // Use Eloquent
+        $request->validate([
+            'name' => 'required',
+            'status' => 'required',
+        ]);
+
+        $sellers = Sellers::create(
+            [
+                'name' => $request->name,
+                'status' => $request->status,
+                'updated_at' => Date::now(),
+                'created_at' => Date::now()
+            ]
+        );
+
+        return redirect()->route('index', compact('sellers'))
+            ->with('success', 'Seller created successfully.');
+    }
+
+    public function storeSellerQuery(Request $request) { // Use Query Builder
+        $request->validate([
+            'name' => 'required',
+            'status' => 'required',
+        ]);
+
+        $sellers = DB::table('sellers')->insert(
+            [
+                'name' => $request->name,
+                'status' => $request->status,
+                'updated_at' => Date::now(),
+                'created_at' => Date::now()
+            ]
+        );
+
+        return redirect()->route('index', compact('sellers'))
+            ->with('success', 'Seller created successfully.');
+    }
+
+    public function editSeller($id) {
+        $sellers = Sellers::find($id);
+
+        return view('index', compact('sellers'));
+    }
+
+    public function updateSellerEloquent(Request $request, $id) { // Use Eloquent
+        $request->validate([
+            'name' => 'required',
+            'status' => 'required',
+        ]);
+
+        $sellers = Sellers::find($id);
+        $sellers->name = $request->name;
+        $sellers->status = $request->status;
+        $sellers->updated_at = Date::now();
+        $sellers->save();
+
+        return redirect()->route('index', compact('sellers'))
+            ->with('success', 'Seller updated successfully');
+    }
+
+    public function updateSellerQuery(Request $request, $id) { // Use Query Builder
+        $request->validate([
+            'name' => 'required',
+            'status' => 'required',
+        ]);
+
+        $sellers = DB::table('sellers')
+            ->where('id', $id)
+            ->update(
+                [
+                    'name' => $request->name,
+                    'status' => $request->status,
+                    'updated_at' => Date::now()
+                ]
+            );
+
+        return redirect()->route('index', compact('sellers'))
+            ->with('success', 'Seller updated successfully');
+    }
+
+    public function deleteSeller($id) {
+        $sellers = Sellers::find($id);
+        $sellers->delete();
+
+        return redirect()->route('index', compact('sellers'))
+            ->with('success', 'Seller deleted successfully');
+    }
+
 }
--- H071211045/Tugas IX/app/Http/Controllers/SellerPermissionController.php
+++ H071211045/Tugas IX/app/Http/Controllers/SellerPermissionController.php
@@ -2,9 +2,107 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SellerPermissions;
 use Illuminate\Http\Request;
+use \Carbon\Carbon as Date;
+use Illuminate\Support\Facades\DB;
 
 class SellerPermissionController extends Controller
 {
-    //
+    public function index() {
+        $sellerPerms = SellerPermissions::join('sellers', 'sellers.id', '=', 'seller_permissions.seller_id')
+            ->join('permissions', 'permissions.id', '=', 'seller_permissions.permission_id')
+            ->select('seller_permissions.*', 'sellers.name as seller_name', 'permissions.name as permission_name')
+            ->latest()->paginate(5);
+
+        return view('index', compact('sellerPerms'));
+    }
+
+    public function storeSellerPermsEloquent(Request $request) { // Use Eloquent
+        $request->validate([
+            'seller_id' => 'required',
+            'permission_id' => 'required',
+        ]);
+
+        $sellerPerms = SellerPermissions::create(
+            [
+                'seller_id' => $request->seller_id,
+                'permission_id' => $request->permission_id,
+                'updated_at' => Date::now(),
+                'created_at' => Date::now()
+            ]
+        );
+
+        return redirect()->route('index', compact('sellerPerms'))
+            ->with('success', 'Seller Permission created successfully.');
+    }
+
+    public function storeSellerPermsQuery(Request $request) { // Use Query Builder
+        $request->validate([
+            'seller_id' => 'required',
+            'permission_id' => 'required',
+        ]);
+
+        $sellerPerms = DB::table('seller_permissions')->insert(
+            [
+                'seller_id' => $request->seller_id,
+                'permission_id' => $request->permission_id,
+                'updated_at' => Date::now(),
+                'created_at' => Date::now()
+            ]
+        );
+
+        return redirect()->route('index', compact('sellerPerms'))
+            ->with('success', 'Seller Permission created successfully.');
+    }
+
+    public function editSellerPerms($id) {
+        $sellerPerms = SellerPermissions::find($id);
+
+        return view('index', compact('sellerPerms'));
+    }
+
+    public function updateSellerPermsEloquent(Request $request, $id) { // Use Eloquent
+        $request->validate([
+            'seller_id' => 'required',
+            'permission_id' => 'required',
+        ]);
+
+        $sellerPerms = SellerPermissions::find($id);
+        $sellerPerms->seller_id = $request->seller_id;
+        $sellerPerms->permission_id = $request->permission_id;
+        $sellerPerms->updated_at = Date::now();
+        $sellerPerms->save();
+
+        return redirect()->route('index', compact('sellerPerms'))
+            ->with('success', 'Seller Permission updated successfully.');
+    }
+
+    public function updateSellerPermsQuery(Request $request, $id) { // Use Query Builder
+        $request->validate([
+            'seller_id' => 'required',
+            'permission_id' => 'required',
+        ]);
+
+        $sellerPerms = DB::table('seller_permissions')
+            ->where('id', $id)
+            ->update(
+                [
+                    'seller_id' => $request->seller_id,
+                    'permission_id' => $request->permission_id,
+                    'updated_at' => Date::now()
+                ]
+            );
+
+        return redirect()->route('index', compact('sellerPerms'))
+            ->with('success', 'Seller Permission updated successfully.');
+    }
+
+    public function deleteSellerPerms($id) {
+        $sellerPerms = SellerPermissions::find($id);
+        $sellerPerms->delete();
+
+        return redirect()->route('index', compact('sellerPerms'))
+            ->with('success', 'Seller Permission deleted successfully.');
+    }
 }
