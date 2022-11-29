56c6c476aa4cce797cb0008dffb70eefd3bbd8f2
sofyanox12 <gaero38@gmail.com>
2022-11-29 10:48:26 +0800

    Menambah fungsi pada `CategoryController`

--- H071211045/Tugas IX/app/Http/Controllers/CategoryController.php
+++ H071211045/Tugas IX/app/Http/Controllers/CategoryController.php
@@ -2,8 +2,105 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categories;
+use \Carbon\Carbon as Date;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller {
-    //
+
+    public function index() {
+        $categories = Categories::latest()->paginate(5);
+
+        return view('categories.index', compact('categories'));
+            // ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    public function storeCategoryEloquent(Request $request) { // Use Eloquent
+        $request->validate([
+            'name' => 'required',
+            'status' => 'required',
+        ]);
+
+        $categories = Categories::create(
+            [
+                'name' => $request->name,
+                'status' => $request->status,
+                'updated_at' => Date::now(),
+                'created_at' => Date::now()
+            ]
+        );
+
+        return redirect()->route('categories.index', compact('categories'))
+            ->with('success', 'Category created successfully.');
+    }
+
+    public function storeCategoryQuery(Request $request) { // Use Query Builder
+        $request->validate([
+            'name' => 'required',
+            'status' => 'required',
+        ]);
+
+        $categories = DB::table('categories')->insert(
+            [
+                'name' => $request->name,
+                'status' => $request->status,
+                'updated_at' => Date::now(),
+                'created_at' => Date::now()
+            ]
+        );
+
+        return redirect()->route('categories.index', compact('categories'))
+            ->with('success', 'Category created successfully.');
+    }
+
+    public function editCategory($id) {
+        $categories = Categories::find($id);
+
+        return view('index', compact('categories'));
+    }
+
+    public function updateCategoryEloquent(Request $request, $id) { // Use Eloquent
+        $request->validate([
+            'name' => 'required',
+            'status' => 'required',
+        ]);
+
+        $categories = Categories::find($id);
+        $categories->name = $request->name;
+        $categories->status = $request->status;
+        $categories->updated_at = Date::now();
+        $categories->save();
+
+        return redirect()->route('index', compact('categories'))
+            ->with('success', 'Category updated successfully');
+    }
+
+    public function updateCategoryQuery(Request $request, $id) { // Use Query Builder
+        $request->validate([
+            'name' => 'required',
+            'status' => 'required',
+        ]);
+
+        $categories = DB::table('categories')
+            ->where('id', $id)
+            ->update(
+                [
+                    'name' => $request->name,
+                    'status' => $request->status,
+                    'updated_at' => Date::now()
+                ]
+            );
+
+        return redirect()->route('index', compact('categories'))
+            ->with('success', 'Category updated successfully');
+    }
+
+    public function deleteCategory($id) {
+        $categories = Categories::find($id);
+        $categories->delete();
+
+        return redirect()->route('index', compact('categories'))
+            ->with('success', 'Category deleted successfully');
+    }
 }
