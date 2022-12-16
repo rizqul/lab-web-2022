fe006fcd4b3e9a3474a1fcfba49496e90a800b5f
erwinarif31 <muherwinarif31@gmail.com>
2022-12-16 22:43:00 +0800

    fix `dashboard` module

--- H071211059/Tugas-Blog-System/app/Http/Controllers/DashboardController.php
+++ H071211059/Tugas-Blog-System/app/Http/Controllers/DashboardController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.home');
+        $total_user = count(User::all());
+        $total_article = count(Article::all());
+        $total_likes = count(Article::all());
+
+        $most_likes = Article::withCount('likes')->orderBy('likes_count', 'desc')->take(3)->get();
+        $most_views = Article::orderBy('view_count', 'desc')->take(3)->get();
+        $most_comments = Article::withCount('comments')->orderBy('comments_count', 'desc')->take(3)->get();
+
+        return view('dashboard.home', compact('total_user', 'total_article', 'total_likes', 'most_likes', 'most_views', 'most_comments'));
     }
 
     public function profile()
